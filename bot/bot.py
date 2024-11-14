65dc7bab5cfeb4e9b2186d3bd8b865937cc6906a
Magnús Á. Magnússon <magnusm15@ru.is>
2024-11-14 20:42:23 +0000

    bot

--- bot/bot.py
+++ bot/bot.py
@@ -7,7 +7,6 @@ from discord.ext import commands
 from leaderboard.models import Match
 from .django_commands import registerMatch, createLeague, listLeagues, printRanking, addPlayerToLeague, validate_match_payload, getMatchFromMessage, finish_match
 from asgiref.sync import sync_to_async
-from .secret import BOT_AUTH_TOKEN
 
 
 print("Loaded Discord")
@@ -92,4 +91,4 @@ async def register_error(ctx, error):
 
 
 def start():
-    bot.run(BOT_AUTH_TOKEN)
+    bot.run('XXX')
